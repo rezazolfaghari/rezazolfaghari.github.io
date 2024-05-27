66db4a11fec82022a8562c8741b2b2a5d96ef90f
Reza Zolfaghari <49518305+rezazolfaghari@users.noreply.github.com>
2024-05-27 09:17:38 +0330

    its dist

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the '/client' here
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-        <App />
-);
